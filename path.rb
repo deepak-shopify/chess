3719fc5502f5dc8dd841e265bf4dd423130bcf89
deepak-shopify <deepak.bhimaraju@shopify.com>
2025-02-19 03:17:44 +0530

    delete everything

--- path.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class Path
-  attr_accessor :positions
-
-  def initialize(positions)
-    @positions = positions
-  end
-end
--- pieces/direction.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class Direction
-  VERTICAL = "vertical"
-  HORIZONTAL = "horizontal"
-  DIAGONAL = "diagonal"
-end
